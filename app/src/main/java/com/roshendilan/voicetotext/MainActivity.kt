5c35d0414114af86eb9ffbcd03f14a80e2cff110
Roshendz <zoysa3@me.com>
2022-01-11 00:37:09 +0100

    added speech to text function commit

--- app/src/main/java/com/roshendilan/voicetotext/MainActivity.kt
+++ app/src/main/java/com/roshendilan/voicetotext/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.roshendilan.voicetotext
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.SpeechRecognizer
+import android.widget.Toast
+
+import android.speech.RecognizerIntent
+
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.text.set
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private val REQ_CODE_SPEECH_INPUT = 102
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fbRecordVoice.setOnClickListener {
+            listenSpeechInput()
+        }
+    }
+
+    // Receiver
+    private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val result = it.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                tvSpeechInput.text = result?.get(0).toString()
+            }
+        }
+
+    private fun listenSpeechInput() {
+        if (!SpeechRecognizer.isRecognitionAvailable(this)) {
+            Toast.makeText(this, "Speech recognition is not available on your device", Toast.LENGTH_LONG).show()
+        } else {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say something!")
+            getResult.launch(intent)
+        }
     }
 }
